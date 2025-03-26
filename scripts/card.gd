f00abf38c1d18e0cbd7a51b621feb6e647adf268
pekuja <pekuja@iki.fi>
2025-03-26 22:39:24 +0200

    Switched the cards from UI controls to Sprites
    I initially used UI controls because it seemed like mouse input handling is simpler that way, and that is true, but I came to realize that there aren't good tools for other kinds of collision/overlap checks for UI controls, which meant that I would have either have had to whip up my own logic for that, or rely on the mouse events only. Using only mouse events might have been a reasonable choice, but it feels a bit limiting in terms of design, and doesn't match how Windows Solitaire handles it either, and since that's my inspiration for this project anyway, I wanted to be able to handle card overlap detection in a better way.
    
    It does turn out using Sprites did mean I had to redo the input code, and some of it got a bit hairier than I'd like. In particular, I'm now keeping a list of cards the mouse is hovering over, and I'm checking their Z indices to determine if the card we're clicking on is the top one. This works, but relies on the fact that the cards have different Z indices to begin with, which is why in main.gd I've added setting the Z index when spawning the cards The cards also get their Z indices set when dragged on top of other cards, so that their Z index is one higher than the highest card it's piled on top of. This logic doesn't have a whole lot to do with how the actual game will play, but it does make for a neat enough toy for messing around making piles of cards.

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,6 +1,7 @@
-class_name Card extends Control
+class_name Card extends Node2D
 
-@onready var _textureRect = $TextureRect
+@onready var _sprite2D = $Sprite2D
+@onready var _area2D = $Area2D
 
 enum Suit
 {
@@ -25,6 +26,8 @@ var is_dragging : bool = false
 var dragging_offset : Vector2 = Vector2(0,0)
 var is_face_up = true
 
+static var hovered_cards : Array[Card] = []
+
 func is_red():
 	return suit == Suit.Heart or suit == Suit.Diamond
 	
@@ -34,12 +37,12 @@ func is_black():
 func _ready():
 	var atlasTexture : AtlasTexture = AtlasTexture.new()
 	atlasTexture.atlas = cardAtlas
-	_textureRect.texture = atlasTexture
+	_sprite2D.texture = atlasTexture
 	
 	update_texture()
 	
 func update_texture():
-	var atlasTexture : AtlasTexture = _textureRect.texture
+	var atlasTexture : AtlasTexture = _sprite2D.texture
 	if is_face_up:
 		atlasTexture.region.position.x = (value - 1) * VALUE_INCREMENT_X + CARD_OFFSET_X
 		atlasTexture.region.position.y = suit * SUIT_INCREMENT_Y + CARD_OFFSET_Y
@@ -49,18 +52,33 @@ func update_texture():
 	atlasTexture.region.size.x = CARD_WIDTH
 	atlasTexture.region.size.y = CARD_HEIGHT
 
-func _on_gui_input(event: InputEvent) -> void:
+func _input(event : InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed == false and is_dragging:
+			var max_z = 0
+			for other_area2D : Area2D in _area2D.get_overlapping_areas():
+				var other_card : Card = other_area2D.get_parent()
+				if other_card:
+					max_z = max(other_card.z_index, max_z)
+			z_index = max_z + 1
+			global_position = event.position - dragging_offset
+			is_dragging = false
+	if event is InputEventMouseMotion:
+		if is_dragging:
+			global_position = event.position - dragging_offset
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed == true:
+			for card in hovered_cards:
+				if card.z_index > z_index:
+					return
 			z_index = RenderingServer.CANVAS_ITEM_Z_MAX
 			is_dragging = true
-			dragging_offset = event.position
-			
-		elif event.pressed == false and is_dragging:
-			
-			z_index = 0
-			position += event.position - dragging_offset
-			is_dragging = false
-	elif event is InputEventMouseMotion:
-		if is_dragging:
-			position += event.position - dragging_offset
+			dragging_offset = event.position - global_position
+
+func _on_area_2d_mouse_entered() -> void:
+	hovered_cards.append(self)
+
+func _on_area_2d_mouse_exited() -> void:
+	hovered_cards.erase(self)
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Node2D
 
 @export var _card_scene : PackedScene
 
@@ -6,10 +6,10 @@ func _ready():
 	generate_card_deck()
 	
 func generate_card_deck():
-	generate_card_suit(Card.Suit.Heart, Vector2(0, 0))
-	generate_card_suit(Card.Suit.Diamond, Vector2(0, 256))
-	generate_card_suit(Card.Suit.Club, Vector2(0, 512))
-	generate_card_suit(Card.Suit.Spade, Vector2(0, 768))
+	generate_card_suit(Card.Suit.Heart, Vector2(-400, -400))
+	generate_card_suit(Card.Suit.Diamond, Vector2(-400, 256-400))
+	generate_card_suit(Card.Suit.Club, Vector2(-400, 512-400))
+	generate_card_suit(Card.Suit.Spade, Vector2(-400, 768-400))
 	
 func generate_card_suit(suit : Card.Suit, offset : Vector2):
 	for value in range(0, 13):
@@ -17,6 +17,7 @@ func generate_card_suit(suit : Card.Suit, offset : Vector2):
 		card.suit = suit
 		card.value = value + 1
 		card.is_face_up = (value % 2 == 0)
+		card.z_index = value
 		
 		card.global_position = offset + Vector2(value * 64, 0)
 		
