b82174f2af2d9b11ebb23564f21c2963e14ca33b
pekuja <pekuja@iki.fi>
2025-03-30 10:33:51 +0300

    Added generating a Klondike game board, and some rules for dragging cards on top of each other
    The cards now stick to set piles instead of getting dragged all over the place. You can draw cards from the deck, and you can drag cards on top of each other only if allowed by Klondike rules... there are definitely some bugs though, as you're not supposed to be able to drag entire piles of cards but it still seems to happen, and it seems like the Z indexing can get a bit messed up at times.

--- scripts/card.gd
+++ scripts/card.gd
@@ -14,6 +14,9 @@ enum Suit
 @export var value = 1
 @export var suit : Suit = Suit.Heart
 @export var cardAtlas : Texture2D
+@export var is_face_up = true
+
+signal card_clicked
 
 const CARD_WIDTH = 42
 const CARD_HEIGHT = 60
@@ -21,10 +24,11 @@ const CARD_OFFSET_X = 11
 const CARD_OFFSET_Y = 2
 const SUIT_INCREMENT_Y = 64
 const VALUE_INCREMENT_X = 64
+const PILE_OFFSET = 32
 
 var is_dragging : bool = false
 var dragging_offset : Vector2 = Vector2(0,0)
-var is_face_up = true
+var drag_start_position : Vector2 = Vector2(0,0)
 
 static var hovered_cards : Array[Card] = []
 
@@ -33,6 +37,18 @@ func is_red():
 	
 func is_black():
 	return suit == Suit.Club or suit == Suit.Spade
+	
+func is_legal_drop(other_card : Card):
+	if not is_face_up:
+		return false
+	if not is_top_card():
+		return false
+	if is_black() and other_card.is_black() or is_red() and other_card.is_red():
+		return false
+	if other_card.value != value - 1:
+		return false
+	
+	return true
 
 func _ready():
 	var atlasTexture : AtlasTexture = AtlasTexture.new()
@@ -40,6 +56,18 @@ func _ready():
 	_sprite2D.texture = atlasTexture
 	
 	update_texture()
+	
+func is_draggable():
+	# TODO: Support dragging piles of cards
+	if not is_top_card():
+		return false
+	if not is_face_up:
+		return false
+	return true
+	
+func is_top_card():
+	var child_cards = find_children("*", "Card")
+	return child_cards.is_empty()
 	
 func update_texture():
 	var atlasTexture : AtlasTexture = _sprite2D.texture
@@ -55,13 +83,35 @@ func update_texture():
 func _input(event : InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed == false and is_dragging:
-			var max_z = 0
+			var closest_distance = 0.0
+			var card_to_drop_on : Card = null
 			for other_area2D : Area2D in _area2D.get_overlapping_areas():
 				var other_card : Card = other_area2D.get_parent()
-				if other_card:
-					max_z = max(other_card.z_index, max_z)
-			z_index = max_z + 1
-			global_position = event.position - dragging_offset
+				if other_card and other_card != self:
+					if other_card.is_legal_drop(self):
+						var distance = other_card.global_position.distance_squared_to(global_position)
+						if card_to_drop_on == null or distance < closest_distance:
+							card_to_drop_on = other_card
+							closest_distance = closest_distance
+			
+			
+			var parent = get_parent()
+			if card_to_drop_on:
+				z_index = card_to_drop_on.z_index + 1
+				if parent is Card:
+					parent.is_face_up = true
+					parent.update_texture()
+				parent.remove_child(self)
+				card_to_drop_on.add_child(self)
+				
+				position.x = 0
+				position.y = PILE_OFFSET
+			elif parent is Card:
+				position.x = 0
+				position.y = PILE_OFFSET
+			else:
+				position.x = 0
+				position.y = 0
 			is_dragging = false
 	if event is InputEventMouseMotion:
 		if is_dragging:
@@ -70,12 +120,17 @@ func _input(event : InputEvent) -> void:
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed == true:
-			for card in hovered_cards:
-				if card.z_index > z_index:
-					return
-			z_index = RenderingServer.CANVAS_ITEM_Z_MAX
-			is_dragging = true
-			dragging_offset = event.position - global_position
+			card_clicked.emit()
+			if is_draggable():
+				for card in hovered_cards:
+					if card.z_index > z_index:
+						return
+				z_index = RenderingServer.CANVAS_ITEM_Z_MAX
+				is_dragging = true
+				is_face_up = true
+				drag_start_position = global_position
+				update_texture()
+				dragging_offset = event.position - global_position
 
 func _on_area_2d_mouse_entered() -> void:
 	hovered_cards.append(self)
--- scripts/main.gd
+++ scripts/main.gd
@@ -2,23 +2,93 @@ extends Node2D
 
 @export var _card_scene : PackedScene
 
+@onready var _deck_locator = $Deck
+@onready var _draw_pile_locator = $"Draw Pile"
+@onready var _piles = [
+	$"Pile 1",
+	$"Pile 2",
+	$"Pile 3",
+	$"Pile 4",
+	$"Pile 5",
+	$"Pile 6",
+	$"Pile 7"
+	]
+@onready var _stacks = [
+	$"Stack 1",
+	$"Stack 2",
+	$"Stack 3",
+	$"Stack 4"
+	]
+
+var _card_deck : Array[Card] = []
+var _deck_top_card : Card = null
+
 func _ready():
 	generate_card_deck()
 	
-func generate_card_deck():
-	generate_card_suit(Card.Suit.Heart, Vector2(-400, -400))
-	generate_card_suit(Card.Suit.Diamond, Vector2(-400, 256-400))
-	generate_card_suit(Card.Suit.Club, Vector2(-400, 512-400))
-	generate_card_suit(Card.Suit.Spade, Vector2(-400, 768-400))
+	_card_deck.shuffle()
 	
-func generate_card_suit(suit : Card.Suit, offset : Vector2):
+	var index = 0
+	var pile_index = 0
+	_deck_top_card = null
+	for card in _card_deck:
+		card.z_index = index
+		if pile_index < 7:
+			var pile = _piles[pile_index]
+			if _deck_top_card:
+				card.position.y = Card.PILE_OFFSET
+				_deck_top_card.add_child(card)
+			else:
+				pile.add_child(card)
+			_deck_top_card = card
+			
+			index += 1
+			
+			if index > pile_index:
+				card.is_face_up = true
+				card.update_texture()
+				_deck_top_card = null
+				index = 0
+				pile_index += 1
+			
+		else:
+			if _deck_top_card:
+				_deck_top_card.add_child(card)
+			else:
+				_deck_locator.add_child(card)
+			_deck_top_card = card
+			index += 1
+		
+	_deck_top_card.card_clicked.connect(deck_clicked)
+	
+func deck_clicked():
+	_deck_top_card.card_clicked.disconnect(deck_clicked)
+		
+	var parent = _deck_top_card.get_parent()
+	parent.remove_child(_deck_top_card)
+	_draw_pile_locator.add_child(_deck_top_card)
+	_deck_top_card.is_face_up = true
+	_deck_top_card.update_texture()
+	
+	if parent is Card:
+		print("parent is card")
+		_deck_top_card = parent
+		_deck_top_card.card_clicked.connect(deck_clicked)
+	else:
+		print("parent is ", parent)
+		_deck_top_card = null
+	
+func generate_card_deck():
+	generate_card_suit(Card.Suit.Heart)
+	generate_card_suit(Card.Suit.Diamond)
+	generate_card_suit(Card.Suit.Club)
+	generate_card_suit(Card.Suit.Spade)
+	
+func generate_card_suit(suit : Card.Suit):
 	for value in range(0, 13):
 		var card : Card = _card_scene.instantiate()
 		card.suit = suit
 		card.value = value + 1
-		card.is_face_up = (value % 2 == 0)
-		card.z_index = value
+		card.is_face_up = false
 		
-		card.global_position = offset + Vector2(value * 64, 0)
-		
-		add_child(card)
+		_card_deck.append(card)
