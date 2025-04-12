18993a5a72b01fe75529d9fd344d7ead99fed6b2
pekuja <pekuja@iki.fi>
2025-04-12 22:55:47 +0300

    Moved card input code into its own script and node
    The card input is now handled centrally in a "Card Input" node with its own script. This should be more efficient and feels less kludgey in general, as the collision queries are only done in one place. The logic is mostly the same, but now the currently dragged card is held in a variable instead of modifying the self object.

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,7 +1,7 @@
 class_name Card extends Node2D
 
 @onready var _sprite2D = $Sprite2D
-@onready var _area2D = $Area2D
+@onready var area2D = $Area2D
 @onready var _label = $Label
 
 enum Suit
@@ -37,10 +37,6 @@ const VALUE_INCREMENT_X = 64
 const PILE_OFFSET = 56
 
 var _is_dragging : bool = false
-var _dragging_touch_index = -1
-var dragging_offset : Vector2 = Vector2(0,0)
-
-static var is_a_card_being_dragged = false
 
 func is_red():
 	return suit == Suit.Heart or suit == Suit.Diamond
@@ -124,6 +120,9 @@ func is_dragging():
 		
 	return false
 	
+func set_dragging(value : bool):
+	_is_dragging = value
+	
 func update_texture():
 	var atlasTexture : AtlasTexture = _sprite2D.texture
 	if is_face_up:
@@ -134,92 +133,6 @@ func update_texture():
 		atlasTexture.region.position.y = 1 * SUIT_INCREMENT_Y + CARD_OFFSET_Y
 	atlasTexture.region.size.x = CARD_WIDTH
 	atlasTexture.region.size.y = CARD_HEIGHT
-
-func _input(event : InputEvent) -> void:
-	if event is InputEventScreenTouch:
-		if event.is_pressed():
-			# only one drag allowed at once
-			if is_a_card_being_dragged:
-				return
-				
-			if not is_draggable():
-				return
-			
-			var view_to_world = get_canvas_transform().affine_inverse()
-			var touch_position = view_to_world * event.position
-			var space_state = get_world_2d().direct_space_state
-			var query = PhysicsPointQueryParameters2D.new()
-			query.position = touch_position
-			query.collide_with_areas = true
-			var results = space_state.intersect_point(query)
-			var absolute_z_index = get_absolute_z_index()
-			
-			var self_intersected = false
-			
-			for result in results:
-				var area2D = result.collider
-				var card = area2D.get_parent()
-				if card is Card:
-					if card == self:
-						self_intersected = true
-					elif card.get_absolute_z_index() > absolute_z_index:
-						return
-			
-			if not self_intersected:
-				return
-			
-			z_index = RenderingServer.CANVAS_ITEM_Z_MAX
-			_is_dragging = true
-			_dragging_touch_index = event.index
-			is_a_card_being_dragged = true
-			is_face_up = true
-			update_texture()
-			dragging_offset = touch_position - global_position
-		if event.index == _dragging_touch_index and event.pressed == false and _is_dragging:
-			z_index = 1
-			var closest_distance = 0.0
-			var card_to_drop_on = null
-			for other_area2D : Area2D in _area2D.get_overlapping_areas():
-				var other_card_or_pile = other_area2D.get_parent()
-				if (other_card_or_pile is Card or other_card_or_pile is CardPile) and other_card_or_pile != self:
-					if other_card_or_pile.is_legal_drop(self):
-						var distance = other_card_or_pile.global_position.distance_squared_to(global_position)
-						if card_to_drop_on == null or distance < closest_distance:
-							card_to_drop_on = other_card_or_pile
-							closest_distance = distance
-							
-			
-			var parent = get_parent()
-			if card_to_drop_on:
-				if parent is Card:
-					parent.is_face_up = true
-					parent.update_texture()
-				parent.remove_child(self)
-				card_to_drop_on.add_child(self)
-				location = card_to_drop_on.location
-				
-				position.x = 0
-				if card_to_drop_on.location == Location.Tableau and card_to_drop_on is Card:
-					position.y = PILE_OFFSET
-				else:
-					position.y = 0
-					
-			elif parent is Card:
-				position.x = 0
-				if parent.location == Location.Tableau:
-					position.y = PILE_OFFSET
-				else:
-					position.y = 0
-			else:
-				position.x = 0
-				position.y = 0
-			_is_dragging = false
-			is_a_card_being_dragged = false
-	elif event is InputEventScreenDrag:
-		if _is_dragging:
-			var view_to_world = get_canvas_transform().affine_inverse()
-			var touch_position = view_to_world * event.position
-			global_position = touch_position - dragging_offset
 
 func get_absolute_z_index() -> int:
 	var parent = get_parent()
---
+++ scripts/card_input.gd
@@ -0,0 +1,93 @@
+extends Node2D
+
+var _dragging_touch_index = -1
+var dragging_offset : Vector2 = Vector2(0,0)
+var is_a_card_being_dragged = false
+var dragged_card : Card = null
+
+func _input(event : InputEvent) -> void:
+	if event is InputEventScreenTouch:
+		if event.is_pressed():
+			# only one drag allowed at once
+			if is_a_card_being_dragged:
+				return
+			
+			var view_to_world = get_canvas_transform().affine_inverse()
+			var touch_position = view_to_world * event.position
+			var space_state = get_world_2d().direct_space_state
+			var query = PhysicsPointQueryParameters2D.new()
+			query.position = touch_position
+			query.collide_with_areas = true
+			var results = space_state.intersect_point(query)
+			
+			var top_card : Card = null
+			var top_card_z_index = -1
+			
+			for result in results:
+				var area2D = result.collider
+				var card = area2D.get_parent()
+				if card is Card:
+					var card_z_index = card.get_absolute_z_index()
+					if card_z_index > top_card_z_index:
+						top_card = card
+						top_card_z_index = card_z_index
+			
+			if top_card == null:
+				return
+			
+			if not top_card.is_draggable():
+				return
+				
+			dragged_card = top_card
+			
+			dragged_card.z_index = RenderingServer.CANVAS_ITEM_Z_MAX
+			dragged_card.set_dragging(true)
+			_dragging_touch_index = event.index
+			is_a_card_being_dragged = true
+			dragged_card.update_texture()
+			dragging_offset = touch_position - dragged_card.global_position
+		if event.index == _dragging_touch_index and event.pressed == false and dragged_card:
+			dragged_card.z_index = 1
+			var closest_distance = 0.0
+			var card_to_drop_on = null
+			for other_area2D : Area2D in dragged_card.area2D.get_overlapping_areas():
+				var other_card_or_pile = other_area2D.get_parent()
+				if (other_card_or_pile is Card or other_card_or_pile is CardPile) and other_card_or_pile != dragged_card:
+					if other_card_or_pile.is_legal_drop(dragged_card):
+						var distance = other_card_or_pile.global_position.distance_squared_to(dragged_card.global_position)
+						if card_to_drop_on == null or distance < closest_distance:
+							card_to_drop_on = other_card_or_pile
+							closest_distance = distance
+							
+			
+			var parent = dragged_card.get_parent()
+			if card_to_drop_on:
+				if parent is Card:
+					parent.is_face_up = true
+					parent.update_texture()
+				parent.remove_child(dragged_card)
+				card_to_drop_on.add_child(dragged_card)
+				dragged_card.location = card_to_drop_on.location
+				
+				dragged_card.position.x = 0
+				if card_to_drop_on.location == Card.Location.Tableau and card_to_drop_on is Card:
+					dragged_card.position.y = Card.PILE_OFFSET
+				else:
+					dragged_card.position.y = 0
+					
+			elif parent is Card:
+				dragged_card.position.x = 0
+				if parent.location == Card.Location.Tableau:
+					dragged_card.position.y = Card.PILE_OFFSET
+				else:
+					dragged_card.position.y = 0
+			else:
+				dragged_card.position.x = 0
+				dragged_card.position.y = 0
+			dragged_card.set_dragging(false)
+			is_a_card_being_dragged = false
+			dragged_card = null
+	elif event is InputEventScreenDrag and dragged_card:
+		var view_to_world = get_canvas_transform().affine_inverse()
+		var touch_position = view_to_world * event.position
+		dragged_card.global_position = touch_position - dragging_offset
